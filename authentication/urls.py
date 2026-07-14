91dbf04e2b14c964948d973db3b389150e950e2b
Santosh B <sbohara579@gmail.com>
2026-07-14 15:30:48 +0545

    changes: added refresh view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import login_view, logout_view, register_view
+from rest_framework_simplejwt.views import  TokenRefreshView
 
 urlpatterns = [
     path('auth/login',login_view.LoginView.as_view(), name='login' ),
-    path('auth/login',register_view.RegisterView.as_view(), name='login' ),
-    path('auth/login',logout_view.LogoutView.as_view(), name='login' )
+    path('auth/register',register_view.RegisterView.as_view(), name='register' ),
+    path('auth/logout',logout_view.LogoutView.as_view(), name='logout' ),
+    path('auth/refresh',TokenRefreshView.as_view(), name='token_refresh' )
 ]
